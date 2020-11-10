33406aae0851a392cfe1b2c7da7a11d3dce25f4c
Anastasiya Ulitina <anastulitina@gmail.com>
2020-11-10 21:13:33 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Анастасия Улитина on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
