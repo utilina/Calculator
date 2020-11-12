6ecc69d630dd0ff5373acd169445294fb58b91c8
Anastasiya Ulitina <anastulitina@gmail.com>
2020-11-12 20:25:21 +0300

    Dot checkout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isFinishTypingNumber: Bool = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishTypingNumber = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert string to a double")
+        }
+            if let calcMethod = sender.currentTitle {
+                if calcMethod == "+/-" {
+                    displayLabel.text = String(format:"%.0f", number * -1)
+                } else if calcMethod == "AC" {
+                    displayLabel.text = String(format:"%.0f", 0)
+                } else if calcMethod == "%" {
+                    displayLabel.text = String(number / 100)
+                }
+            }
+        
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            
+            if isFinishTypingNumber {
+                displayLabel.text! = numValue
+                isFinishTypingNumber = false
+            } else {
+                
+                if  numValue == "." {
+                    guard let currentDisplayLabel = Double(displayLabel.text!) else { return }
+                    let isInt = floor(currentDisplayLabel) == currentDisplayLabel
+                    print(floor(currentDisplayLabel))
+                    print(isInt)
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text! += numValue
+            }
+            
+        }
+    
+    }
+    
 
 }
 
